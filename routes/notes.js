8ec407a9f7dc86da60dbb8e8199d10cce9c9d8ca
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-28 18:23:00 -0400

    Added post routes for adding new notes

--- routes/notes.js
+++ routes/notes.js
@@ -7,3 +7,34 @@ const { readFile, writeFile }  = require('fs/promises');
 notes.get('/api/notes', (req, res) => {
   readFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
+
+// Post route for adding new notes
+notes.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4().slice()
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+          err ? console.error(err) : console.info(`\nData written to db.json file.`)
+        )
+      } 
+      res.status(200).json('Your note was successfully added')
+    })
+
+  } else {
+    res.status(400).json('There was an error while trying to add the recent note')
+  }
+})
