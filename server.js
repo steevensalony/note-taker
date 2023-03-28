0c75eec80fe14a9f98a2bd6e6dc020751d82cd88
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-28 18:07:27 -0400

    Added different paths to app

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.use('/', api);
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
 });
